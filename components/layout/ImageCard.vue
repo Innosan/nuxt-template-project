7bf74c1b850859a31b8a448d240c5fcaa94e78ef
Innosan <reau.beua@gmail.com>
2024-11-16 17:53:15 +0300

    Dynamic image loading with skeleton

--- components/layout/ImageCard.vue
+++ components/layout/ImageCard.vue
@@ -13,18 +13,33 @@ const props = defineProps({
 		required: true,
 	},
 });
+
+const isLoading = ref(true);
+
+const onLoad = () => {
+	isLoading.value = false;
+};
 </script>
 
 <template>
 	<UCard class="transition-all hover:scale-105 hover:shadow-md">
 		<template #header>
-			<NuxtImg
-				loading="lazy"
-				class="h-56 object-top rounded-2xl content-center w-full object-cover hover:cursor-zoom-in"
-				:src="url"
-				:alt="'Neko art by' + alt"
-				@click="modal.open(FullSizeImage, { url: url, alt: alt })"
-			/>
+			<div class="relative h-56">
+				<USkeleton
+					class="absolute inset-0 bg-gray-200 rounded-2xl transition-opacity duration-300"
+					:class="{ 'opacity-0': !isLoading }"
+				/>
+
+				<NuxtImg
+					class="absolute inset-0 h-56 object-top rounded-2xl content-center w-full object-cover hover:cursor-zoom-in transition-opacity duration-300"
+					:class="{ 'opacity-0': isLoading }"
+					loading="lazy"
+					:src="url"
+					:alt="'Neko art by' + alt"
+					@load="onLoad"
+					@click="modal.open(FullSizeImage, { url: url, alt: alt })"
+				/>
+			</div>
 		</template>
 
 		<slot />
