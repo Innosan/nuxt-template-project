0770ddbe61762f511754dc46917f3053b2df2411
Innosan <reau.beua@gmail.com>
2025-04-21 15:13:51 +0300

    Refactor: Simplify AppHeader layout by adjusting header structure

--- components/utility/AppHeader.vue
+++ components/utility/AppHeader.vue
@@ -4,11 +4,11 @@ const appConfig = useAppConfig();
 
 <template>
 	<header class="flex justify-between items-center gap-4">
-		<div class="flex gap-6 items-center">
-			<h1 class="font-black text-2xl text-nowrap text-primary-400">
-				{{ appConfig.appName }}
-			</h1>
+		<h1 class="font-black text-2xl text-nowrap text-primary-400">
+			{{ appConfig.appName }}
+		</h1>
 
+		<div class="flex gap-3 items-center">
 			<AppNavigation :title="appConfig.appName as string" />
 
 			<LanguageSwitcher />
